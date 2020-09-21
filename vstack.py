3c039580c9f4c07b36bde4675d61fd55db55fa4c
kirandk02 <70264092+kirandk02@users.noreply.github.com>
2020-09-21 02:23:50 -0700

    Add files via upload

---
+++ vstack.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 14:53:24 2020
+
+@author: KIRAN HP
+"""
+
+import numpy as np
+sem3=np.array([51,59,51,55,60,54,91,91])
+sem4=np.array([53,49,50,55,55,56,69,76])
+print(np.vstack((sem3,sem4)))
